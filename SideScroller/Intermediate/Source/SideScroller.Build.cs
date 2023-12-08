a47080defd1ecb7049a9316ff179b5fb35027a94
Pedro Tello <Tello.3810@outlook.com>
2023-12-08 00:50:21 -0600

    Correccion del juego

---
+++ SideScroller/Intermediate/Source/SideScroller.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class SideScroller : ModuleRules
+{
+	public SideScroller(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
