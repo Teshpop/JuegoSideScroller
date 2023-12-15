7ece7ee8aaad4444d39a6605da366b461359b5d2
Pedro Tello <Tello.3810@outlook.com>
2023-12-15 10:49:53 -0600

    Revert "merge Desarrollo"
    This reverts commit 5f939a8432a92309fdf06a4c96dbd490210ab88a, reversing
    changes made to 3c91446e3da32a89eee73a69dfda0fd08d8c4012.

---
+++ SideScroller/Intermediate/Build/Win64/x64/SideScroller/DebugGame/Core/SharedPCH.Core.NonOptimized.ShadowErrors.InclOrderUnreal5_0.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.37.32824
+#include "D:/Documentos/UNREAL/JuegoSideScroller/SideScroller/Intermediate/Build/Win64/x64/SideScroller/DebugGame/Core/SharedPCH.Core.NonOptimized.ShadowErrors.InclOrderUnreal5_0.h"
---
+++ SideScroller/Intermediate/Build/Win64/x64/SideScroller/Development/Core/SharedPCH.Core.ShadowErrors.InclOrderUnreal5_0.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.37.32824
+#include "D:/Documentos/UNREAL/JuegoSideScroller/SideScroller/Intermediate/Build/Win64/x64/SideScroller/Development/Core/SharedPCH.Core.ShadowErrors.InclOrderUnreal5_0.h"
